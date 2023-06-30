ba80320441a11e96898d5c3f194f5d4f9a32f2b8
iwint <iwint.cseasnsct@gmail.com>
2023-07-01 00:02:47 +0530

    env file changed to secrets

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,15 @@
-import { createApi, fetchBaseQuery, } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { API_URL } from '../../secret';
 const baseQuery = fetchBaseQuery({
-    baseUrl: process.env.API_URL,
+  baseUrl: API_URL,
 });
 const baseQueryWithInterceptor = async (args, api, extraOptions) => {
-    let result = await baseQuery(args, api, extraOptions);
-    if (result.error && result.error.status === 401) {
-    }
-    return result;
+  let result = await baseQuery(args, api, extraOptions);
+  if (result.error && result.error.status === 401) {
+  }
+  return result;
 };
 export const api = createApi({
-    baseQuery: baseQueryWithInterceptor,
-    endpoints: () => ({}),
+  baseQuery: baseQueryWithInterceptor,
+  endpoints: () => ({}),
 });
